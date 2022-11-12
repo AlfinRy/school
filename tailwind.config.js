4223d987e8274fff6f0d3a6128437a82aa8b8df1
DillonVantra <88568109+DillonVantra@users.noreply.github.com>
2022-11-12 12:49:14 +0700

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     extend: {
       colors: {
-        primary : '#043463',
-        secondary : '#ba060c',
-        three : '#d9a668',
-      } 
+        primary: "#043463",
+        secondary: "#ba060c",
+        three: "#d9a668",
+      },
+    },
+    fontFamily: {
+      Quicksand: ["Quicksand", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
     },
   },
   plugins: [],
-}
+};
 // npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
