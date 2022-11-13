5c1bd0c1c9ac879558bbf9891bc40bd187ba6efd
AlfinRy <alfin.reynaldi26@gmail.com>
2022-11-13 23:12:42 +0700

    fixed navbar and profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  content: ['index.html'],
-  theme: {
-    extend: {
-      colors: {
-        primary: 	'#b91c1c',
-        secondary: '#ba060c',
-        three: '#d9a668',
-      },
-    },
-    fontFamily: {
-      Quicksand: ["Quicksand", "sans-serif"],
-      Poppins: ["Poppins", "sans-serif"],
-    },
-  },
-  plugins: [],
+	content: ['index.html'],
+	theme: {
+		extend: {
+			colors: {
+				secondary: '#ffba75',
+				primary: '#b91c1c',
+				three: '#d9a668',
+			},
+		},
+		fontFamily: {
+			Quicksand: ['Quicksand', 'sans-serif'],
+			Poppins: ['Poppins', 'sans-serif'],
+		},
+	},
+	plugins: [],
 };
 // npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
